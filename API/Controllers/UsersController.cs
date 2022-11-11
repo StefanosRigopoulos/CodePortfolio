d8766c64dcdd19089643a14fa90d7f3d5cfb3bbb
Stefanos <stefarigop@gmail.com>
2022-11-11 16:13:13 +0200

    Walking Skeleton Part 2 - Angular & BootStrap

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -20,7 +20,7 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GeatUsers()
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();
         }
