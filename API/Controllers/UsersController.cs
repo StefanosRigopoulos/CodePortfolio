49cc3858c121cc925e5af76ffadf846b8a3f5649
Stefanos <stefarigop@gmail.com>
2023-01-02 19:01:01 +0200

    Authentication Basics

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,17 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    [Authorize]
+    public class UsersController : BaseAPIController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
@@ -19,6 +15,7 @@ namespace API.Controllers
             _context = context;
         }
 
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -11,7 +11,6 @@ namespace API.Data
     {
         public DataContext(DbContextOptions options) : base(options)
         {
-
         }
 
         public DbSet<AppUser> Users { get; set; }
